da47fceae45fbb2b2a6752befd516b01d1a7071f
CIPHERTron <pritish.samal918@gmail.com>
2021-02-24 01:51:43 +0530

    Add Login And Token Verification Controllers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -42,6 +42,23 @@ const user = {
 			return res.status(500).json({ msg: err.message });
 		}
 	},
+ verifiedToken: (req, res) => {
+  try {
+   const token = req.header("Authorization")
+   if(!token) return res.send(false)
+
+   jwt.verify(token, process.env.SECRET_TOKEN, async (err, verified) => {
+    if(err) return res.send(false)
+
+    const user = await Users.findById(verified.id)
+    if(!user) return res.send(false)
+
+    return res.send(true)
+   })
+  } catch (err) {
+   return res.status(500).json({msg: err.message})
+  }
+ }
 };
 
 module.exports = user;
