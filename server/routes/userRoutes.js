3d0e4db4ab4466f7ce290e045e0f74914093d452
CIPHERTron <pritish.samal918@gmail.com>
2021-02-23 23:23:06 +0530

    Add User Signup and Login Routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,10 @@
 const router = require('express').Router();
+const userController = require('../controllers/user.js')
 
 // Register User
-router.post('/register', (req, res) => {
-	res.json({ msg: 'Register a User' });
-});
+router.post('/register', userController.registerUser);
 
 // Login User
-router.post('/login', (req, res) => {
-	res.json({ msg: 'User Login' });
-});
+router.post('/login', userController.loginUser);
 
 module.exports = router;
