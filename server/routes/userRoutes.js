d6d3717b53000a694a7430eed9df06e432bbee1c
CIPHERTron <pritish.samal918@gmail.com>
2021-02-24 01:52:25 +0530

    Add Verify Token Route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const userController = require('../controllers/user.js')
+const auth = require('../middlewares/auth.js')
 
 // Register User
 router.post('/register', userController.registerUser);
 
 // Login User
 router.post('/login', userController.loginUser);
+
+// Verify Token
+router.get('/verify', userController.verifiedToken)
 
 module.exports = router;
