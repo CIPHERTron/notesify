7b633510a5bd7551d8e6f51d296c4c685f5e3e1b
CIPHERTron <pritish.samal918@gmail.com>
2021-03-17 23:35:45 +0530

    client: Setup client routes

--- client/src/components/Notes.js
+++ client/src/components/Notes.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Header from './notes/Nav';
+import Home from './notes/Home';
+import CreateNote from './notes/CreateNote';
+import EditNote from './notes/EditNote';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-export default function Notes() {
- return (
-  <div>
-   Notes Component
-  </div>
- )
+export default function Notes({setIsLogin}) {
+	return (
+  <Router>
+   <div className="notes-page">
+    <Header setIsLogin={setIsLogin} />
+    <section>
+     <Route path="/" exact component={Home} />
+     <Route path="/create" exact component={CreateNote} />
+     <Route path="/edit/:id" exact component={EditNote} />
+    </section>
+   </div>
+  </Router>
+ );
 }
