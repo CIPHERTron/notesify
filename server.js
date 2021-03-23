112a1dba3e0ae00a16ab6c3fe45ecf92a572790a
CIPHERTron <pritish.samal918@gmail.com>
2021-03-24 02:11:11 +0530

    deploy: MOdify Server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path')
 
 const userRoutes = require('./routes/userRoutes.js');
 const notesRoutes = require('./routes/notesRoutes.js');
@@ -13,12 +14,6 @@ app.use(express.json());
 
 app.use('/user', userRoutes);
 app.use('/api/notes', notesRoutes);
-
-// Listen Server
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-	console.log('Server running at port: ', PORT);
-});
 
 // Connection to MongoDB Atlas
 const DB_URI = process.env.DATABASE_URL;
@@ -35,6 +30,21 @@ mongoose.connect(
 		console.log('Connected to MongoDB Cluster');
 	}
 );
+
+
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+	})
+}
+
+
+// Listen Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log('Server running at port: ', PORT);
+});
 
 // const MongoClient = require('mongodb').MongoClient;
 // const DB_URI = process.env.DATABASE_URL;
